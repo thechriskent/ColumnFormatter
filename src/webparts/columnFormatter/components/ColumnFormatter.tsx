d127dd0af5b5bb510c48601a7b3a462a3dd2553d
thechriskent <ihatetimemachines@gmail.com>
2017-12-05 11:59:15 -0500

    Added a non working commandbar

--- src/webparts/columnFormatter/components/ColumnFormatter.tsx
+++ src/webparts/columnFormatter/components/ColumnFormatter.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import styles from './ColumnFormatter.module.scss';
 import { IColumnFormatterProps } from './IColumnFormatterProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import { CommandBar } from 'office-ui-fabric-react/lib/CommandBar';
 var SplitPane = require('react-split-pane');
 
 import ColumnFormatterPropertyPane from './Panes/PropertyPane/ColumnFormatterPropertyPane';
@@ -11,15 +12,38 @@ export default class ColumnFormatter extends React.Component<IColumnFormatterPro
   public render(): React.ReactElement<IColumnFormatterProps> {
     return (
       <div className={styles.columnFormatter}>
-        <SplitPane
-         split="vertical"
-         className={styles.SplitPane}
-         size={185}
-         minSize={100}
-         maxSize={-200}>
-          <ColumnFormatterPropertyPane/>
-          <ColumnFormatterViewPane/>
-        </SplitPane>
+        <CommandBar
+          items={[
+            {
+              key: 'new',
+              name: 'New',
+              iconProps: {iconName: 'Add'}
+            },
+            {
+              key: 'customize',
+              name: 'Customize',
+              iconProps: {iconName: 'Fingerprint'},
+            }
+          ]}
+          farItems={[
+            {
+              key: 'copy',
+              name: 'Copy',
+              iconProps: {iconName: 'Copy'}
+            }
+          ]}
+        />
+        <div className={styles.app}>
+          <SplitPane
+          split="vertical"
+          className={styles.SplitPane}
+          size={185}
+          minSize={100}
+          maxSize={-200}>
+            <ColumnFormatterPropertyPane/>
+            <ColumnFormatterViewPane/>
+          </SplitPane>
+        </div>
       </div>
     );
   }
