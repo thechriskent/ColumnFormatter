35cc9aafcec821e1c9a787d74835c5eb433d6a1c
thechriskent <ihatetimemachines@gmail.com>
2017-12-10 15:05:11 -0500

    Refactored dataReducer and added value generation to column type switching

--- src/webparts/columnFormatter/state/Reducers.ts
+++ src/webparts/columnFormatter/state/Reducers.ts
@@ -1,51 +1,70 @@
-import { IApplicationState, initialState, columnTypes } from "./State";
-import { ActionTypes, typeKeys } from "./Actions";
+import { IApplicationState, initialState, columnTypes, IColumn, IData } from "./State";
+import { 
+	ActionTypes, typeKeys, IUpdateDataRowAction, 
+	IUpdateDataColumnNameAction, IUpdateDataColumnTypeAction
+} from "./Actions";
 import { clone } from '@microsoft/sp-lodash-subset';
 import { generateRowValue } from './ValueGeneration';
 
 export const dataReducer = (state:IApplicationState = initialState, action:ActionTypes): IApplicationState => {
 	let newState:IApplicationState = clone(state);
 
-	/*console.log(generateRowValue(columnTypes.text));
-	console.log(generateRowValue(columnTypes.person));
-	console.log(generateRowValue(columnTypes.number));
-	console.log(generateRowValue(columnTypes.datetime));
-	console.log(generateRowValue(columnTypes.lookup));
-	console.log(generateRowValue(columnTypes.link));
-	console.log(generateRowValue(columnTypes.picture));
-	console.log(generateRowValue(columnTypes.boolean));*/
-
 	switch (action.type) {
 		case typeKeys.UPDATE_DATA_ROW:
-			newState.data.rows = [
-				...newState.data.rows.slice(0,action.rowIndex),
-				[
-					...newState.data.rows[action.rowIndex].slice(0,action.colIndex),
-					action.value,
-					...newState.data.rows[action.rowIndex].slice(action.colIndex+1)],
-				...newState.data.rows.slice(action.rowIndex+1)];
-			return newState;
+			newState.data.rows = UpdateDataRowReducer(newState.data.rows, action);
+			break;
 		case typeKeys.UPDATE_DATA_COLUMN_NAME:
-			newState.data.columns = [
-				...newState.data.columns.slice(0,action.colIndex),
-				{
-					...newState.data.columns[action.colIndex],
-					name: action.name
-				},
-				...newState.data.columns.slice(action.colIndex+1)
-			];
-			return newState;
+			newState.data.columns = UpdateDataColumnNameReducer(newState.data.columns, action);
+			break;
 		case typeKeys.UPDATE_DATA_COLUMN_TYPE:
-			newState.data.columns = [
-				...newState.data.columns.slice(0,action.colIndex),
-				{
-					...newState.data.columns[action.colIndex],
-					type: action.colType
-				},
-				...newState.data.columns.slice(action.colIndex+1)
-			];
+			newState.data = UpdateDataColumnTypeReducer(newState.data, action);
 			return newState;
 		default:
 			return state;
 	}
+	return newState;
 };
+
+//** Changes the value of the specified row and column */
+function UpdateDataRowReducer(rows:Array<Array<any>>, action:IUpdateDataRowAction): Array<Array<any>> {
+	return [
+		...rows.slice(0, action.rowIndex),
+		[
+			...rows[action.rowIndex].slice(0, action.colIndex),
+			action.value,
+			...rows[action.rowIndex].slice(action.colIndex + 1)],
+		...rows.slice(action.rowIndex + 1)
+	];
+}
+
+//** Changes the name of the specified column */
+function UpdateDataColumnNameReducer(columns:Array<IColumn>, action:IUpdateDataColumnNameAction): Array<IColumn> {
+	return [
+		...columns.slice(0, action.colIndex),
+		{
+			...columns[action.colIndex],
+			name: action.name
+		},
+		...columns.slice(action.colIndex + 1)
+	];
+}
+
+//** Changes the type of the specified column (and regenerates the row values as well) */
+function UpdateDataColumnTypeReducer(data:IData, action:IUpdateDataColumnTypeAction): IData {
+	return {
+		columns: [
+			...data.columns.slice(0, action.colIndex),
+			{
+				...data.columns[action.colIndex],
+				type: action.colType
+			},
+			...data.columns.slice(action.colIndex + 1)
+		],
+		rows: data.rows.map((row:Array<any>, rIndex:number) => {
+			return [
+				...row.slice(0, action.colIndex),
+				generateRowValue(action.colType),
+				...row.slice(action.colIndex + 1)];
+		})
+	};
+}
