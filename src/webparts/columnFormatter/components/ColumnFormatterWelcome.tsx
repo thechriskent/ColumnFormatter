ddf499c3b752cddddab2b50b642dee93630e7c8b
thechriskent <ihatetimemachines@gmail.com>
2017-12-17 22:38:12 -0500

    Loading from List works!

--- src/webparts/columnFormatter/components/ColumnFormatterWelcome.tsx
+++ src/webparts/columnFormatter/components/ColumnFormatterWelcome.tsx
@@ -4,26 +4,30 @@ import * as strings from 'ColumnFormatterWebPartStrings';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { autobind } from 'office-ui-fabric-react/lib/Utilities';
-import { IApplicationState, columnTypes } from '../state/State';
+import { IApplicationState, columnTypes, IContext } from '../state/State';
 import { launchEditor, launchEditorWithCode } from '../state/Actions';
-import { iconForType, textForType } from '../helpers/ColumnTypeHelpers';
+import { iconForType, typeForTypeAsString, textForType } from '../helpers/ColumnTypeHelpers';
 import { Icon } from 'office-ui-fabric-react/lib/Icon';
 import { DefaultButton, PrimaryButton } from 'office-ui-fabric-react/lib/Button';
 import { Dropdown, IDropdownOption } from 'office-ui-fabric-react/lib/Dropdown';
 import { Label } from 'office-ui-fabric-react/lib/Label';
 import { ChoiceGroup, IChoiceGroupOption } from 'office-ui-fabric-react/lib/ChoiceGroup';
+import { Spinner, SpinnerSize } from 'office-ui-fabric-react/lib/Spinner';
 import { IWizard, Wizards, getWizardByName, getWizardsForColumnType } from './Wizards/WizardCommon';
 import { select } from 'glamor';
 import { FileUploader } from './FileUploader';
+import pnp from "sp-pnp-js";
 
 export enum welcomeStage {
   start,
   new,
   open,
-  upload
+  upload,
+  loadFromList
 }
 
 export interface IColumnFormatterWelcomeProps {
+  context?: IContext;
   launchEditor?: (wizardName:string, colType:columnTypes) => void;
   launchEditorWithCode?: (wizardName:string, colType:columnTypes, editorString:string, validationErrors:Array<string>) => void;
 }
@@ -36,6 +40,12 @@ export interface IColumnFormatterWelcomeState {
   loadChoiceForOpen?: string;
   columnTypeForOpen?: columnTypes;
   fileChoiceForOpen?: string;
+  listsLoaded: boolean;
+  lists: Array<any>;
+  selectedList?: string;
+  selectedField?: string;
+  loadingFromList: boolean;
+  loadFromListError?: string;
 }
 
 class ColumnFormatterWelcome_ extends React.Component<IColumnFormatterWelcomeProps, IColumnFormatterWelcomeState> {
@@ -45,7 +55,10 @@ class ColumnFormatterWelcome_ extends React.Component<IColumnFormatterWelcomePro
 
     this.state = {
       stage: welcomeStage.start,
-      useWizardForNew: true
+      useWizardForNew: true,
+      listsLoaded: false,
+      lists: new Array<any>(),
+      loadingFromList: false
     };
   }
 
@@ -193,6 +206,47 @@ class ColumnFormatterWelcome_ extends React.Component<IColumnFormatterWelcomePro
               </div>
             </div>
           )}
+          {this.state.stage == welcomeStage.loadFromList && (
+            <div>
+              {!this.props.context.isOnline && (
+                <span>This feature is not available from the local workbench</span>
+              )}
+              {!this.state.listsLoaded && this.props.context.isOnline && !this.state.loadingFromList && this.state.loadFromListError == undefined && (
+                <Spinner size={SpinnerSize.large} label='Loading Lists...'/>
+              )}
+              {this.state.listsLoaded && this.props.context.isOnline && !this.state.loadingFromList && this.state.loadFromListError == undefined && (
+                <div>
+                  <Dropdown
+                  label='Local list'
+                  selectedKey={this.state.selectedList}
+                  onChanged={(item:IDropdownOption)=> {this.setState({selectedList: item.key.toString(),selectedField: undefined});}}
+                  required={true}
+                  options={this.listsToOptions()} />
+                  <Dropdown
+                  label='Field'
+                  selectedKey={this.state.selectedField}
+                  disabled={this.state.selectedList == undefined}
+                  onChanged={(item:IDropdownOption)=> {this.setState({selectedField: item.key.toString()});}}
+                  required={true}
+                  options={this.fieldsToOptions()} />
+                </div>
+              )}
+              {this.state.loadingFromList && this.state.loadFromListError == undefined &&(
+                <Spinner size={SpinnerSize.large} label='Loading from list...'/>
+              )}
+              {this.state.loadFromListError !== undefined && (
+                <span className={styles.errorMessage}>{this.state.loadFromListError}</span>
+              )}
+              <div className={styles.navigationButtons}>
+                <div>
+                  <DefaultButton text={strings.WelcomeBackButton} onClick={() => {this.gotoStage(welcomeStage.open);}}/>
+                </div>
+                <div style={{textAlign: 'right'}}>
+                  <PrimaryButton text={strings.WelcomeOKButton} disabled={!this.okButtonEnabled()} onClick={this.onOkForLoadFromListClick}/>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     );
@@ -216,6 +270,11 @@ class ColumnFormatterWelcome_ extends React.Component<IColumnFormatterWelcomePro
         return (
           this.state.loadChoiceForOpen == 'list' ||
           (this.state.loadChoiceForOpen == 'file' && this.state.columnTypeForOpen !== undefined && this.state.fileChoiceForOpen !== undefined)
+        );
+      case welcomeStage.loadFromList:
+        return (
+          this.props.context.isOnline && this.state.selectedList !== undefined &&
+            this.state.selectedField !== undefined && this.state.loadFromListError == undefined
         );
       default:
         return false;
@@ -312,7 +371,7 @@ class ColumnFormatterWelcome_ extends React.Component<IColumnFormatterWelcomePro
   @autobind
   private onOkForOpenClick(): void {
     if(this.state.loadChoiceForOpen == 'list'){
-
+      this.gotoLoadFromList();
     } else {
       if(this.state.fileChoiceForOpen == 'library'){
 
@@ -324,21 +383,114 @@ class ColumnFormatterWelcome_ extends React.Component<IColumnFormatterWelcomePro
 
   @autobind
 	private onFileTextReceived(fileText:string) {
+    this.launchEditorFromText(fileText, this.state.columnTypeForOpen || columnTypes.text);
+  }
+
+  private launchEditorFromText(text:string, type:columnTypes): void {
     //TODO: Check for wizard details in file
     let validationErrors:Array<string> = new Array<string>();
     try {
-      let curObj:any = JSON.parse(fileText);
+      let curObj:any = JSON.parse(text);
     } catch (e) {
       validationErrors.push(e.message);
     }
-    this.props.launchEditorWithCode(undefined, this.state.columnTypeForOpen || columnTypes.text, fileText, validationErrors);
-	}
+    this.props.launchEditorWithCode(undefined, type, text, validationErrors);
+  }
+  
+  private gotoLoadFromList(): void {
+    if(!this.state.listsLoaded) {
+      if(this.props.context.isOnline) {
+        pnp.sp.web.lists.filter('Hidden eq false').select('Id','Title','Fields/InternalName','Fields/TypeAsString','Fields/Hidden','Fields/Title','Fields/DisplayFormat').expand('Fields').get()
+          .then((data:any) => {
+            let listdata:Array<any> = new Array<any>();
+            for(var i=0; i<data.length; i++){
+              listdata.push({
+                Id: data[i].Id,
+                Title: data[i].Title,
+                Fields: data[i].Fields.map((field:any, index:number) => {
+                  if(!field.Hidden) {
+                    let ftype = typeForTypeAsString(field.TypeAsString, field.DisplayFormat);
+                    if(ftype !== undefined) {
+                      return {
+                        Title: field.Title,
+                        InternalName: field.InternalName,
+                        Type: ftype
+                      };
+                    }
+                  }
+                }).filter((field:any, index:number) => {return field !== undefined;})
+              });
+            }
+
+            this.setState({
+              listsLoaded: true,
+              lists: listdata
+            });
+          })
+          .catch((error:any) => {
+            this.setState({
+              loadFromListError: 'Error while loading lists! Technical Details: ' + error.message
+            });
+          });
+      }
+    }
+    this.gotoStage(welcomeStage.loadFromList);
+  }
+
+  private listsToOptions(): Array<IDropdownOption> {
+    let items:Array<IDropdownOption> = new Array<IDropdownOption>();
+    for(var list of this.state.lists) {
+      items.push({
+        key: list.Id,
+        text: list.Title
+      });
+    }
+    return items;
+  }
+
+  private fieldsToOptions(): Array<IDropdownOption> {
+    let items:Array<IDropdownOption> = new Array<IDropdownOption>();
+    for(var list of this.state.lists) {
+      if(list.Id == this.state.selectedList) {
+        for(var field of list.Fields) {
+          items.push({
+            key: field.InternalName,
+            text: field.Title + ' [' + textForType(field.Type) + ']'
+          });
+        }
+        break;
+      } 
+    }
+    return items;
+  }
+
+  @autobind
+  private onOkForLoadFromListClick(): void {
+    this.setState({
+      loadingFromList: true,
+      loadFromListError: undefined
+    });
+    pnp.sp.web.lists.getById(this.state.selectedList)
+      .fields.getByInternalNameOrTitle(this.state.selectedField).select('CustomFormatter','TypeAsString','DisplayFormat').get()
+      .then((data)=>{
+        this.launchEditorFromText(data.CustomFormatter, typeForTypeAsString(data.TypeAsString, data.DisplayFormat));
+        this.setState({
+          loadingFromList: false,
+        });
+      })
+      .catch((error:any) => {
+        this.setState({
+          loadingFromList: false,
+          loadFromListError: 'Error while loading! Technical Details: ' + error.message
+        });
+      });
+  }
 
 }
 
 function mapStateToProps(state: IApplicationState): IColumnFormatterWelcomeProps{
 	return {
-
+    context: state.context,
 	};
 }
 
